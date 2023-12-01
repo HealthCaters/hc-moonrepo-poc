32d6b7ae5bf0b425818812a6d1a9403e77616cb7
Carlos Lozano Sánchez <carlos.lozano@healthcaters.de>
2023-12-01 16:01:46 +0100

    add vite 5

---
+++ apps/vite5/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'node:path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@hc/shared': path.join(__dirname, '../../packages/shared/src/'),
+      '@hc/vite5': path.join(__dirname, 'src/'),
+    },
+  },
+});
